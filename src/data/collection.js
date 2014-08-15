127d28e5333821bc4e47bdf9ee6e6a6ca6d8a334
Timo Weiß <info@timo-weiss.com>
2014-08-15 10:50:33 +0200

    set slash if necessary in constructor

--- src/data/collection.js
+++ src/data/collection.js
@@ -18,6 +18,9 @@
 Bikini.Collection = Backbone.Collection.extend({
 
     constructor: function (options) {
+        if(this.url && this.url.charAt(this.url.length - 1) !== '/') {
+            this.url += '/';
+        }
         this.init(options);
         Backbone.Collection.apply(this, arguments);
     }
