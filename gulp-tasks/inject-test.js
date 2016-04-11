2423d6b0136607970679082c3bef39be17ac53d6
DrMabuse23 <p.brewing@mwaysolutions.com>
2016-04-11 17:09:08 +0200

    using inject

--- gulp-tasks/inject-test.js
+++ gulp-tasks/inject-test.js
@@ -11,7 +11,7 @@ gulp.task('bower-test-inject', function () {
 gulp.task('inject-test', ['bower-test-inject'], function () {
   gulp.src('./test/test.html')
   .pipe(inject(
-    gulp.src(['./test/**/*.js', '!./test/js/**/*.js'], {read: false}), {
+    gulp.src(['./test/**/*.js', '!./test/js/**/*.js', '!./test/backbone/**/*.js', '!./test/test.js'], {read: false}), {
       transform: function (filepath) {
         filepath = filepath.replace('/test', '.');
         return inject.transform.apply(inject.transform, arguments);
